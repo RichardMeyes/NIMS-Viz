ba82740fdc25bb6b0db1543a2c5123bdd01d60e1
UniMiguel <1242703@uni-wuppertal.de>
2019-06-16 19:18:46 +0200

    improve designe

--- static/frontend/src/app/entities/threeDObject.ts
+++ static/frontend/src/app/entities/threeDObject.ts
@@ -1,8 +1,27 @@
 import * as THREE from 'three';
+import "three/examples/js/loaders/OBJLoader2.js";
 
 export class threeDObject {
 
+    private _scene: THREE.Scene;
+    public get scene(): THREE.Scene {
+        return this._scene;
+    }
+
+    private _renderer: THREE.WebGLRenderer;
+    public get renderer(): THREE.WebGLRenderer {
+        return this._renderer;
+    }
+
     constructor() {
-        
+
+    }
+
+    private loadObject() {
+        const objLoader = new THREE.OBJLoader2();
+        objLoader.load('/assets/models/obj/Brain_Model_2.obj', (event) => {
+            const root = event.detail.loaderRootNode;
+            this._scene.add(root);
+        });
     }
 }
