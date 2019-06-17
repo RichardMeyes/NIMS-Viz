ed3d41896630d90ba7c9d6791c784c5383e0cee6
UniMiguel <1242703@uni-wuppertal.de>
2019-06-17 07:19:32 +0200

    disable threeDObject because not working atm

--- static/frontend/src/app/entities/threeDObject.ts
+++ static/frontend/src/app/entities/threeDObject.ts
@@ -1,27 +1,29 @@
-import * as THREE from 'three';
-import "three/examples/js/loaders/OBJLoader2.js";
+// import * as THREE from 'three';
 
-export class threeDObject {
+// import "three/examples/js/loaders/OBJLoader2.js";
+// import "three/examples/js/loaders/LoaderSupport.js";
 
-    private _scene: THREE.Scene;
-    public get scene(): THREE.Scene {
-        return this._scene;
-    }
+// export class threeDObject {
 
-    private _renderer: THREE.WebGLRenderer;
-    public get renderer(): THREE.WebGLRenderer {
-        return this._renderer;
-    }
+//     private _scene: THREE.Scene;
+//     public get scene(): THREE.Scene {
+//         return this._scene;
+//     }
 
-    constructor() {
+//     private _renderer: THREE.WebGLRenderer;
+//     public get renderer(): THREE.WebGLRenderer {
+//         return this._renderer;
+//     }
 
-    }
+//     constructor() {
 
-    private loadObject() {
-        const objLoader = new THREE.OBJLoader2();
-        objLoader.load('/assets/models/obj/Brain_Model_2.obj', (event) => {
-            const root = event.detail.loaderRootNode;
-            this._scene.add(root);
-        });
-    }
-}
+//     }
+
+//     private loadObject() {
+//         const objLoader = new THREE.OBJLoader2();
+//         objLoader.load('/assets/models/obj/Brain_Model_2.obj', (event) => {
+//             const root = event.detail.loaderRootNode;
+//             this._scene.add(root);
+//         });
+//     }
+// }
