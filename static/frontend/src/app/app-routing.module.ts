3ae59be0e778fc4957d2f5316aeeee670248c0a1
vrenaldi <vrenaldi258@gmail.com>
2019-08-08 20:22:56 +0200

    Remove ablation-view, move Ablate Network to menu-elements.

--- static/frontend/src/app/app-routing.module.ts
+++ static/frontend/src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { BrainViewComponent } from './components/brain-view/brain-view.component';
-import { AblationViewComponent } from './components/ablation-view/ablation-view.component';
 import { LayerViewComponent } from './components/layer-view/layer-view.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'brain-view', component: BrainViewComponent},
-  {path: 'ablation-view', component: AblationViewComponent},
   {path: 'layer-view', component: LayerViewComponent}
 ];
 
@@ -21,6 +19,5 @@ export class AppRoutingModule { }
 export const routingComponents = 
   [LandingPageComponent, 
     BrainViewComponent,
-    AblationViewComponent,
     LayerViewComponent
   ];
