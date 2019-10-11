44a5d2ef013d31e8e7233fddd26362d90e9daa05
vrenaldi <vrenaldi258@gmail.com>
2019-10-11 16:01:06 +0200

    Reset network when leaving ablation mode.

--- static/frontend/src/app/components/network-ablator/network-ablator.component.ts
+++ static/frontend/src/app/components/network-ablator/network-ablator.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { BackendCommunicationService } from 'src/app/backendCommunication/backend-communication.service';
 
 @Component({
   selector: 'app-network-ablator',
   templateUrl: './network-ablator.component.html',
   styleUrls: ['./network-ablator.component.scss']
 })
-export class NetworkAblatorComponent implements OnInit {
+export class NetworkAblatorComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private backend: BackendCommunicationService
+  ) { }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.backend.resetAblation().subscribe();
   }
 
 }
